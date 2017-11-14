a17fe24762fa2a36fd36bccbb4a15449a955d8f4
MSW98 <MSW98@DESKTOP-4BCNEIF>
2017-11-14 20:19:29 +0700

    IF11_10116488_LatihanUTS3

---
+++ src/if11_10116488_latihanuts3/IF11_10116488_LatihanUTS3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if11_10116488_latihanuts3;
+
+import view.Volunter;
+
+/**
+ *
+ * @author MSW98
+ */
+public class IF11_10116488_LatihanUTS3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Volunter nVol = new Volunter();
+        nVol.setVisible(true);
+    }
+    
+}
